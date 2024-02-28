8a7c6de020d0289707a412990bdc5354135ec2e3
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-02-27 23:24:37 -0600

    Making responsive navbar and aboutus
    Co-authored-by: Legan-Aminora <Changeek2@outlook.com>

--- app/Opinions/page.tsx
+++ app/Opinions/page.tsx
@@ -6,17 +6,17 @@ const Page: React.FC = () => {
         <div className="cards sm:flex-col md:flex-row">
             <div className="person-card">
                 <Image className="person-image" src="/images/2.jpg" alt="Foto de cliente" width={100} height={100} />
-                <text className="person-name">Lucia D.</text>
+                <div className="person-name">Lucia D.</div>
                 <p className="opinion">Honestamente, recibí el mejor servicio que pude haber pedido. Fueron súper profesionales a la hora de tratar; cumplieron todas mis necesidades y me ayudaron mucho.</p>
             </div>
             <div className="person-card">
                 <Image className="person-image" src="/images/2.jpg" alt="Foto de cliente" width={100} height={100} />
-                <text className="person-name">Martha W.</text>
+                <div className="person-name">Martha W.</div>
                 <p className="opinion">La plataforma me brindó toda la asistencia necesaria, fácil de utilizar e intuitiva.</p>
             </div>
             <div className="person-card">
                 <Image className="person-image" src="/images/2.jpg" alt="Foto de cliente" width={100} height={100} />
-                <text className="person-name">Juan F.</text>
+                <div className="person-name">Juan F.</div>
                 <p className="opinion">Facilita mucho el proceso para encontrar mobiliario confiable, además de que ahora puedo recomendar este sitio a conocidos de cualquier lado en la república.</p>
             </div>
         </div>
